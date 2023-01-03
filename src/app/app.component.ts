36bcfeacb273f11d135c3e8ddab6561ac98c8bda
Rene Rivas <rrivas@techinsights.com>
2023-01-03 15:19:18 -0400

    Established PrimeNg and project layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+interface Persona {
+  nombre: string;
+  apellido: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  public nombre: string = 'ReNe RIVas';
+  public cedula: number = 18938896;
+  public persona: Persona = {
+    nombre: 'Rene',
+    apellido: 'Rivas',
+  };
+
+  public mostrarNombre(): void {
+    console.log(this.nombre);
+    console.log(this.cedula);
+    console.log(this.persona);
+  }
 }

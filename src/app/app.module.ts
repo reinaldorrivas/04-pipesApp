603d3e42d1514fcc4f57c1d0566c00eb8a2a2f23
Rene Rivas <rrivas@techinsights.com>
2023-01-04 14:05:35 -0400

    Created dummy module for PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
-// PrimeNg
-import { ButtonModule } from 'primeng/button';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ButtonModule],
+  imports: [BrowserModule, PrimeNgModule],
   providers: [],
   bootstrap: [AppComponent],
 })
